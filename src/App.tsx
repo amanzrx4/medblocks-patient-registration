dd421ffefe81c1488cfabfcd84d4adbd791c708c
aman sagar <amansagar262@gmail.com>
2025-05-19 16:19:58 +0530

    feat: add shadcn & tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import './App.css'
+import Home from './pages/Home'
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return <Home />
 }
 
 export default App
