268b56c410b92eeadb8a47ffbb5ca5b3d09a8086
aman sagar <amansagar262@gmail.com>
2025-05-23 17:42:52 +0530

    feat: add seeding

--- src/components/TestComp.tsx
+++ src/components/TestComp.tsx
@@ -1,18 +1,24 @@
+import { queries } from '@/db/queries'
 import { useLiveQuery, usePGlite } from '@electric-sql/pglite-react'
 import { Loader2 } from 'lucide-react'
 import { useState } from 'react'
 import { Button } from './ui/button'
+import { Input } from './ui/input'
+import { seedData } from '@/db/seed'
+import type { PGliteWorkerWithLive } from '@/App'
 
 export default function TestComp() {
-  const db = usePGlite()
+  const db = usePGlite() as PGliteWorkerWithLive
+  const [count, setCount] = useState('')
   const [isAdding, setIsAdding] = useState(false)
 
-  const records = useLiveQuery(`SELECT * FROM users;`)
+  const records = useLiveQuery(queries.prod.selectAll)
 
   async function addRecords() {
     setIsAdding(true)
     try {
-      await db.query("INSERT INTO users (name) VALUES ('Alice')")
+      await seedData(Number(count), db)
+      console.log(`seeding successfull`)
     } catch (e) {
       console.error('Error in insert', e)
     } finally {
@@ -21,11 +27,21 @@ export default function TestComp() {
   }
 
   return (
-    <div>
+    <div className="flex flex-col gap-2 items-center">
+      <Input
+        value={count}
+        onChange={(e) => {
+          const value = e.target.value
+
+          if (!isNaN(Number(value)) && value.trim() !== '') {
+            setCount(value)
+          }
+        }}
+      />
       <Button onClick={addRecords} disabled={isAdding}>
         {isAdding && <Loader2 className="animate-spin" />}
 
-        {isAdding ? 'Adding...' : 'Add records'}
+        {isAdding ? 'Seeding...' : 'Seed records'}
       </Button>
 
       <h1 className="text-3xl">LEADER: {db.isLeader}</h1>
---
+++ src/db/seed.ts
@@ -0,0 +1,90 @@
+import type { PGliteWorkerWithLive } from '@/App'
+import { faker } from '@faker-js/faker'
+
+export async function seedData(count: number, db: PGliteWorkerWithLive) {
+  for (let i = 0; i < count; i++) {
+    const registrationDateTime = new Date().toISOString()
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+    const sex = faker.helpers.arrayElement(['male', 'female', 'other'])
+    const dob = faker.date.anytime().toISOString().split('T')[0]
+    const phoneNumber = '+91' + '0000000000'
+    const email = faker.internet.email({ firstName, lastName }).toLowerCase()
+    const addressLine1 = faker.location.streetAddress()
+    const addressLine2 = faker.location.secondaryAddress()
+    const city = faker.location.city()
+    const state = faker.location.state()
+    const postalCode = faker.location.zipCode()
+    const reason = faker.lorem.sentence()
+    const additionalNotes = faker.lorem.paragraph()
+    const patientHistory = faker.lorem.paragraphs(2)
+    const keyValuePairs = {
+      insurance: faker.person.firstName(),
+      emergency_contact: faker.person.fullName()
+    }
+
+    const query = `
+  INSERT INTO patients (
+    registration_datetime,
+    key_value_pairs,
+    first_name,
+    last_name,
+    sex,
+    dob,
+    phone_number,
+    email,
+    address_line1,
+    address_line2,
+    city,
+    state,
+    postal_code,
+    reason,
+    additional_notes,
+    patient_history
+  ) VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8,
+    $9, $10, $11, $12, $13, $14, $15, $16
+  );
+`
+
+    const values = [
+      registrationDateTime,
+      JSON.stringify(keyValuePairs),
+      firstName,
+      lastName,
+      sex,
+      dob,
+      phoneNumber,
+      email,
+      addressLine1,
+      addressLine2,
+      city,
+      state,
+      postalCode,
+      reason,
+      additionalNotes,
+      patientHistory
+    ]
+
+    console.log(`inserting data`, {
+      registrationDateTime,
+      firstName,
+      lastName,
+      sex,
+      dob,
+      phoneNumber,
+      email,
+      addressLine1,
+      addressLine2,
+      city,
+      state,
+      postalCode,
+      reason,
+      additionalNotes,
+      patientHistory,
+      keyValuePairs
+    })
+
+    await db.query(query, values)
+  }
+}
