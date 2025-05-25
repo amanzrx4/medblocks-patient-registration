82b0c0f2552d0d67b327a9037cd7584b0ebd11cd
aman sagar <amansagar262@gmail.com>
2025-05-26 01:16:51 +0530

    feat: render json results with table

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,25 @@ export const REPOSITORY_GITHUB_URL =
   'https://github.com/amanzrx4/medblocks-patient-registration'
 
 export const DB_NAME = 'patients'
+
+export const patientTableKeys = [
+  'id',
+  'registration_datetime',
+  'key_value_pairs',
+  'first_name',
+  'last_name',
+  'sex',
+  'dob',
+  'phone_number',
+  'email',
+  'address_line1',
+  'address_line2',
+  'city',
+  'state',
+  'postal_code',
+  'reason',
+  'additional_notes',
+  'patient_history',
+  'photo',
+  'created_at'
+] as const
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,5 @@
 import { queries } from '@/db/queries'
-import type { PGliteWorkerWithLive } from '@/utils'
+import type { PatientTable, PGliteWorkerWithLive } from '@/utils'
 import * as XLSX from 'xlsx'
 import { DB_NAME } from '@/utils'
 import { live } from '@electric-sql/pglite/live'
@@ -68,4 +68,48 @@ export function getFormattedDate(dt: Date) {
 
 export function scrollToTop() {
   document.body.scrollTop = document.documentElement.scrollTop = 0
+}
+
+/**
+ *
+ * @param arr Array of objects
+ * @description Removes the keys that consistently have null values in all objects of the array
+ */
+export function cleanArray<T>(arr: T[]) {
+  const nullKeys = Object.keys(arr[0] || {}).filter((k) =>
+    arr.every((o) => o[k as keyof T] === null)
+  )
+  return arr.map((o) =>
+    Object.fromEntries(
+      Object.entries(o as Record<string, unknown>).filter(
+        ([k]) => !nullKeys.includes(k)
+      )
+    )
+  )
+}
+
+/**
+ *
+ * @param key string key
+ * @returns formatted postgres table key into readable format
+ */
+export function formatKey(key: string) {
+  return key.replace(/_/g, ' ').replace(/\b\w/g, (c) => c.toUpperCase())
+}
+
+export function formatValueForObj(key: keyof PatientTable, value: unknown) {
+  if (value === null || value === undefined) return ''
+
+  switch (key) {
+    case 'photo':
+      return value ? '✅' : '❌'
+    case 'registration_datetime':
+    case 'dob':
+    case 'created_at':
+      return getFormattedDate(new Date(value as string))
+    case 'key_value_pairs':
+      return JSON.stringify(value)
+    default:
+      return String(value)
+  }
 }
